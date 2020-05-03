5c0fa0db7661bf0d463b6dd93f9605583bd19fec
Rohan Prajapati <58733914+Jackrohan003@users.noreply.github.com>
2020-05-03 22:09:48 +0530

    Update ArrayListMethods.java

--- ArrayListMethods.java
+++ ArrayListMethods.java
@@ -8,15 +8,24 @@ class ArrayListMethods{
     
     public static void stringExample()
      {
-     ArrayList<String> alist=new ArrayList<String>();  
-     alist.add("Gregor Clegane");  
-     alist.add("Khal Drogo");  
-     alist.add("Cersei Lannister");  
-     alist.add("Sandor Clegane"); 
-     alist.add("Tyrion Lannister");
-  
-     //iterating ArrayList
-     for(String str:alist)  
-        System.out.println(str);  
+       ArrayList<String> books = new ArrayList<>();
+        
+        //Adding element to the list (add Method)
+        books.add("Java");
+        books.add("C++");
+        books.add("Data Structure");
+        books.add("Unix");
+        books.add("Discrete Maths");
+        
+        System.out.println(books);
+
+        //Adding at the specific postion
+        books.add(3,"Vb.net");      //this will add at 4th postion and index 3
+        System.out.println(books);
+        
+        //Change an element at specific postion
+        books.set(0,"Java With Data Structure");
+        System.out.println(books);
+        
      }    
 }
