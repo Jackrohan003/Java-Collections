adb159745fa6818bfbce4ad859fd13d5cc9f7bc8
Rohan Prajapati <58733914+Jackrohan003@users.noreply.github.com>
2020-05-03 22:28:16 +0530

    Update ArrayListMethods.java

--- ArrayListMethods.java
+++ ArrayListMethods.java
@@ -8,7 +8,8 @@ class ArrayListMethods{
     
     public static void stringExample()
      {
-       ArrayList<String> books = new ArrayList<>();
+     
+         ArrayList<String> books = new ArrayList<>();
         
         //Adding element to the list (add Method)
         books.add("Java");
@@ -20,12 +21,39 @@ class ArrayListMethods{
         System.out.println(books);
 
         //Adding at the specific postion
-        books.add(3,"Vb.net");      //this will add at 4th postion and index 3
+        books.add(3,"Vb.net");          //this will add at 4th postion and index 3
         System.out.println(books);
         
         //Change an element at specific postion
-        books.set(0,"Java With Data Structure");
+        books.set(0,"Java With Data Structure");    // if index is not there then Exception will occur
         System.out.println(books);
+        
+        //Remove element from ArrayList
+        books.remove("Vb.net");             //if specified element is not there then nothing will happen
+        System.out.println(books);
+        
+        //Remove 3rd element 
+        books.remove(2);       // 3rd postion 2nd index
+        System.out.println(books);    
+            
+        
+        //Iterating ArrayList Using For Each Method
+        for(String str:books)
+            System.out.print(str+" ");
+        System.out.println();
+        
+        //Size of the ArrayList size() Method
+        System.out.println(books.size());
+        
+        //Common For Loop for ArrayList
+        
+        for(int i=0;i<books.size();i++)
+            System.out.println(books.get(i));
+        
+        //Sort the list using Collections class. This is part of java.util package 
+        Collections.sort(books);
+        System.out.println(books);
+        
         
      }    
 }
