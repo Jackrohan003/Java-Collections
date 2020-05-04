d67aa5cbaa20e8c670a8bc119c1b37f429e3a650
Rohan Prajapati <58733914+Jackrohan003@users.noreply.github.com>
2020-05-04 10:32:04 +0530

    Update ArrayListMethods.java

--- ArrayListMethods.java
+++ ArrayListMethods.java
@@ -12,6 +12,7 @@ class ArrayListMethods{
          ArrayList<String> books = new ArrayList<>();
         
         //Adding element to the list (add Method)
+        
         books.add("Java");
         books.add("C++");
         books.add("Data Structure");
@@ -21,29 +22,32 @@ class ArrayListMethods{
         System.out.println(books);
 
         //Adding at the specific postion
+        
         books.add(3,"Vb.net");          //this will add at 4th postion and index 3
         System.out.println(books);
         
         //Change an element at specific postion
+        
         books.set(0,"Java With Data Structure");    // if index is not there then Exception will occur
         System.out.println(books);
         
         //Remove element from ArrayList
+        
         books.remove("Vb.net");             //if specified element is not there then nothing will happen
         System.out.println(books);
         
         //Remove 3rd element 
+        
         books.remove(2);       // 3rd postion 2nd index
         System.out.println(books);    
             
-        
         //Iterating ArrayList Using For Each Method
         for(String str:books)
             System.out.print(str+" ");
         System.out.println();
         
-        //Size of the ArrayList size() Method
-        System.out.println(books.size());
+          //Size of the ArrayList size() Method
+          System.out.println(books.size());
         
         //Common For Loop for ArrayList
         
@@ -51,9 +55,21 @@ class ArrayListMethods{
             System.out.println(books.get(i));
         
         //Sort the list using Collections class. This is part of java.util package 
+    
         Collections.sort(books);
         System.out.println(books);
         
+         //Check Wheather element is present or not
+        
+        System.out.println(books.contains("Unix"));
+        System.out.println(books.contains("Algorithm"));
+        
+        
+        //Sorting reverse Order
+        
+        Collections.sort(books,Collections.reverseOrder());
+        System.out.println(books);
+      
         
      }    
 }
